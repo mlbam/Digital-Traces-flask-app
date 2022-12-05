741b513a05da6b71c03abd9ddaa286a9ffad89b5
mlbam <myra-louise.bamde@epfedu.fr>
2022-12-05 16:38:00 +0100

    streamit server running locally

--- myappGoogleAnalytics/main.py
+++ myappGoogleAnalytics/main.py
@@ -1,7 +1,2 @@
-from flask import Flask, render_template
-app = Flask(__name__)
-
-@app.route('/', methods=["GET"])
-def hello_world():
-    # display the home_page.html template
-    return render_template('home_page.html')
+import streamlit as st
+st.title("Hello world")

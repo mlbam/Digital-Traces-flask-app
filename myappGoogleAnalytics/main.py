e179b8f3d068b2528ad6c064d8f1a9baee892d19
mlbam <myra-louise.bamde@epfedu.fr>
2022-12-05 15:26:29 +0100

    modification of the html

--- myappGoogleAnalytics/main.py
+++ myappGoogleAnalytics/main.py
@@ -1,18 +1,7 @@
-from flask import Flask
-
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/', methods=["GET"])
 def hello_world():
-    prefix_google = """
-    <!-- Google tag (gtag.js) -->
-    <script async
-    src="https://www.googletagmanager.com/gtag/js?id=UA-251017585-1"></script>
-    <script>
-    window.dataLayer = window.dataLayer || [];
-    function gtag(){dataLayer.push(arguments);}
-    gtag('js', new Date());
-    gtag('config', ' UA-251017585-1');
-    </script>
-    """
-    return prefix_google + "Hello World"
+    # display the home_page.html template
+    return render_template('home_page.html')

81243a9e665756258b2c5a28be59a288c9e39ccb
mlbam <myra-louise.bamde@epfedu.fr>
2022-12-07 14:38:02 +0100

    added the logs part

--- myappGoogleAnalytics/main.py
+++ myappGoogleAnalytics/main.py
@@ -5,3 +5,12 @@ app = Flask(__name__)
 def hello_world():
     # display the home_page.html template
     return render_template('home_page.html')
+
+# add a route to see the logs 
+@app.route('/logger', methods=["GET"])
+def logs():
+    # print a log on python 
+    app.logger.info('This is a log')
+    # display the logs on the front 
+    return "This is a log"
+

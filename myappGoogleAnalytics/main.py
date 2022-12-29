8855681fe515c37b8bb2f10c341240d2ee6a6b97
mlbam <myra-louise.bamde@epfedu.fr>
2022-12-29 10:19:04 +0100

    exo 2 tp2

--- myappGoogleAnalytics/main.py
+++ myappGoogleAnalytics/main.py
@@ -10,6 +10,8 @@ def hello_world():
 # add a route to see the logs 
 @app.route('/logger', methods=["GET"])
 def logger():
+    # print in the console a phrase with a smiley hand shaking
+    print("Hello from the console ")
     script = """
     <script> console.log("Hello, just a little phrase in a console 😋")</script>"""
     return "Take a look at the console for a surprise 🎉" + script
